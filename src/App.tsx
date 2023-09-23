a5324895b18df07e6b0e8da1510571afd7526f55
Jvnino <jvnino@uniboyaca.edu.co>
2023-09-22 21:18:15 -0500

    CSS arreglado
    Se añadió mas CSS para mejorar la apariencia de la pagina

--- src/App.tsx
+++ src/App.tsx
@@ -11,43 +11,51 @@ function App() {
     setOperacion(operacion=>operacion+simbolo)
   }
 
+  //Funcion calcular resultado
   const calcularResultado = ()=>{
     setResultado(eval(operacion))
-    setOperacion("")
+
   }
 
   return (
     <>
-      <section>
-        <input type="text" value={operacion} disabled/> =
-        <label htmlFor="">{resultado}</label>
-      </section>
+    <div id='general'>
+      
+        {/* Div con las grid */}
+        <div id='gridContainer'>
 
-    <div>
+          {/* Seccion que contiene el label y el input con el resultado */}
+          <section id='label'>
+            <input type="text" value={operacion} disabled/>
+            <label htmlFor="">= {resultado}</label>
+          </section>
 
-      <section id='numeros'>
-      <button onClick={()=>agregarOperacion("1")}>1</button>
-      <button onClick={()=>agregarOperacion("2")}>2</button>
-      <button onClick={()=>agregarOperacion("3")}>3</button>
-      <button onClick={()=>agregarOperacion("4")}>4</button>
-      <button onClick={()=>agregarOperacion("5")}>5</button>
-      <button onClick={()=>agregarOperacion("6")}>6</button>
-      <button onClick={()=>agregarOperacion("7")}>7</button>
-      <button onClick={()=>agregarOperacion("8")}>8</button>
-      <button onClick={()=>agregarOperacion("9")}>9</button>
-      <button id='cero' onClick={()=>agregarOperacion("0")}>0</button>
-      </section>
+          {/* Seccion con los números de la calculadora */}
+          <section id='numeros'>
+          <button onClick={()=>agregarOperacion("1")}>1</button>
+          <button onClick={()=>agregarOperacion("2")}>2</button>
+          <button onClick={()=>agregarOperacion("3")}>3</button>
+          <button onClick={()=>agregarOperacion("4")}>4</button>
+          <button onClick={()=>agregarOperacion("5")}>5</button>
+          <button onClick={()=>agregarOperacion("6")}>6</button>
+          <button onClick={()=>agregarOperacion("7")}>7</button>
+          <button onClick={()=>agregarOperacion("8")}>8</button>
+          <button onClick={()=>agregarOperacion("9")}>9</button>
+          <button id='cero' onClick={()=>agregarOperacion("0")}>0</button>
+          </section>
 
-        <section id='operaciones'>
-          <button onClick={()=>agregarOperacion("+")}>+</button>
-          <button onClick={()=>agregarOperacion("-")}>-</button>
-          <button onClick={()=>agregarOperacion("*")}>*</button>
-          <button onClick={()=>agregarOperacion("/")}>/</button>
-          <button onClick={()=>agregarOperacion("%")}>%</button>
-          <button onClick={calcularResultado}>=</button>
-          <button onClick={()=>setOperacion("")}>C</button>
-        </section>
-        
+          {/* Seccion con las operaciones de la calculadora */}
+          <section id='operaciones'>
+            <button onClick={() => { setOperacion(""); setResultado(0); }}>C</button>
+            <button onClick={()=>agregarOperacion("+")}>+</button>
+            <button onClick={()=>agregarOperacion("-")}>-</button>
+            <button onClick={()=>agregarOperacion("*")}>*</button>
+            <button onClick={()=>agregarOperacion("/")}>/</button>
+            <button onClick={()=>agregarOperacion("%")}>%</button>
+            <button id="igual" onClick={calcularResultado}>=</button>
+          </section>
+
+        </div>
       </div>
     </>
   )
